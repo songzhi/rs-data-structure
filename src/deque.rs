fec43c61398f44f10fccc05fec80d646113d238d
冬冬 李 <lsongzhi@163.com>
2019-03-08 21:04:21 +0800

    push_front

--- src/deque.rs
+++ src/deque.rs
@@ -28,4 +28,23 @@ impl<T> Deque<T> {
     pub fn new() -> Self {
         Deque { head: None, tail: None }
     }
+    pub fn push_front(&mut self, elem: T) {
+        // new node needs +2 links,everything else should be +0
+        let new_head = Node::new(elem);
+        match self.head.take() {
+            Some(old_head) => {
+                // non-empty list,need to connect the old_head
+                old_head.borrow_mut().prev = Some(new_head.clone()); // +1 new_head
+                new_head.borrow_mut().next = Some(old_head); // +1 old_head
+                self.head = Some(new_head); // +1 new_head, -1 old_head
+                // total: +2 new_head, +0 old_head -- OK!
+            }
+            None => {
+                // empty list, need to set the tail
+                self.tail = Some(new_head.clone()); // +1 new_head
+                self.head = Some(new_head) // +1 new_head
+                // total: +2 new_head -- OK
+            }
+        }
+    }
 }
