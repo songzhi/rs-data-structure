13ca5f592274d9c3ad0b5a9dccdada6195ef968a
冬冬 李 <lsongzhi@163.com>
2019-03-08 20:55:28 +0800

    Deque

---
+++ src/deque.rs
@@ -0,0 +1,31 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub struct Deque<T> {
+    head: Link<T>,
+    tail: Link<T>,
+}
+
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+    prev: Link<T>,
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem,
+            prev: None,
+            next: None,
+        }))
+    }
+}
+
+impl<T> Deque<T> {
+    pub fn new() -> Self {
+        Deque { head: None, tail: None }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@
 
 pub mod vec;
 pub mod list;
+pub mod deque;
