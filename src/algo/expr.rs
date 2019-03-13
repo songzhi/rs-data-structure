af70c3c65d343ddc59b22af92d5c28fdc372924e
冬冬 李 <lsongzhi@163.com>
2019-03-13 21:34:07 +0800

    eval_post_expr

--- src/algo/expr.rs
+++ src/algo/expr.rs
@@ -41,14 +41,50 @@ pub fn infix_expr_to_post(tokens: impl Iterator<Item=char>) -> String {
     post_expr
 }
 
+pub fn eval_post_expr(expr: impl Iterator<Item=char>) -> Option<i64> {
+    let mut stack: Vec<i64> = vec![];
+    for ch in expr {
+        match ch {
+            '+' => {
+                let (x, y) = (stack.pop()?, stack.pop()?);
+                stack.push(y + x);
+            }
+            '-' => {
+                let (x, y) = (stack.pop()?, stack.pop()?);
+                stack.push(y - x);
+            }
+            '*' => {
+                let (x, y) = (stack.pop()?, stack.pop()?);
+                stack.push(y * x);
+            }
+            '/' => {
+                let (x, y) = (stack.pop()?, stack.pop()?);
+                stack.push(y / x);
+            }
+            '0'..='9' => {
+                stack.push(ch.to_digit(10)?.into());
+            }
+            _ => { return None; }
+        }
+    }
+    stack.pop()
+}
+
 #[cfg(test)]
 mod test {
-    use crate::algo::expr::infix_expr_to_post;
+    use crate::algo::expr::{infix_expr_to_post, eval_post_expr};
 
     #[test]
     fn test_infix_expr_to_post() {
         let infix_expr = "a+b*c+(d*e+f)*g";
         let postfix_expr = infix_expr_to_post(infix_expr.chars().into_iter());
         assert_eq!("abc*+de*f+g*+", postfix_expr);
+    }
+
+    #[test]
+    fn test_eval_post_expr() {
+        let expr = "1+2*(5-3)";
+        let post_expr = infix_expr_to_post(expr.chars().into_iter());
+        assert_eq!(Some(5), eval_post_expr(post_expr.chars().into_iter()));
     }
 }
