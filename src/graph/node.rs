2bf11d5c961602c6fba36a345b595c315b99ef7d
冬冬 李 <lsongzhi@163.com>
2019-03-14 17:35:51 +0800

    tests

--- src/graph/node.rs
+++ src/graph/node.rs
@@ -33,3 +33,62 @@ impl<'a, N: 'a + NodeTrait> Iterator for Nodes<'a, N> {
         self.iter.size_hint()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::graph::edge::Direction as CompactDirection;
+    use crate::graph::node::Nodes;
+    use indexmap::IndexMap;
+
+    #[test]
+    fn new() {
+        let nodes: IndexMap<i32, Vec<(i32, CompactDirection)>> = IndexMap::new();
+
+        Nodes::new(nodes.keys().cloned());
+    }
+
+    #[test]
+    fn next() {
+        let mut nodes: IndexMap<i32, Vec<(i32, CompactDirection)>> = IndexMap::with_capacity(3);
+        nodes.insert(1, vec![]);
+        nodes.insert(2, vec![]);
+        nodes.insert(3, vec![]);
+
+        let mut nodes = Nodes::new(nodes.keys().cloned());
+
+        // Test node `1`.
+        assert_eq!(nodes.next(), Some(1));
+        assert_eq!(nodes.next(), Some(2));
+        assert_eq!(nodes.next(), Some(3));
+
+        // Test the end of iteration.
+        assert_eq!(nodes.next(), None);
+    }
+
+    #[test]
+    fn size_hint() {
+        let mut nodes: IndexMap<i32, Vec<(i32, CompactDirection)>> = IndexMap::with_capacity(3);
+        nodes.insert(1, vec![]);
+        nodes.insert(2, vec![]);
+        nodes.insert(3, vec![]);
+
+        let mut nodes = Nodes::new(nodes.keys().cloned());
+
+        assert_eq!(nodes.size_hint(), (3, Some(3)));
+
+        // Lower the length of the iterator.
+        nodes.next();
+
+        assert_eq!(nodes.size_hint(), (2, Some(2)));
+
+        // Lower the length of the iterator.
+        nodes.next();
+
+        assert_eq!(nodes.size_hint(), (1, Some(1)));
+
+        // Lower the length of the iterator.
+        nodes.next();
+
+        assert_eq!(nodes.size_hint(), (0, Some(0)));
+    }
+}
