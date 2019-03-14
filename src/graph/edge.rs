7bec7df5be8a9e43ec90123acb8fdf360db1aad1
冬冬 李 <lsongzhi@163.com>
2019-03-14 17:40:26 +0800

    tests

--- src/graph/edge.rs
+++ src/graph/edge.rs
@@ -274,3 +274,230 @@ impl<'a, Ix, E> IntoWeightedEdge<E> for &'a (Ix, Ix, E)
         self.clone()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::graph::edge::{AllEdges, Direction as CompactDirection, Direction, EdgeType, Edges, IntoWeightedEdge};
+    use crate::graph::{Directed, Undirected};
+    use crate::graph::traverse::Neighbors;
+    use indexmap::IndexMap;
+    use std::cmp::PartialEq;
+    use std::marker::PhantomData;
+
+    #[test]
+    fn edge_type_is_directed() {
+        assert_eq!(Directed::is_directed(), true);
+        assert_eq!(Undirected::is_directed(), false);
+    }
+
+    #[test]
+    fn edges_new() {
+        // Prepare arguments.
+        let from: u32 = 1;
+        let edges: IndexMap<(u32, u32), f32> = IndexMap::new();
+        let node_neighbors: Vec<(u32, CompactDirection)> = vec![];
+        let iter = node_neighbors.iter();
+        let neighbors: Neighbors<u32, Directed> = Neighbors::new(iter, PhantomData);
+
+        // Test `Edges` struct creation.
+        Edges::new(from, &edges, neighbors);
+    }
+
+    #[test]
+    fn edges_next() {
+        // Prepare arguments.
+        let from: u32 = 1;
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+        let node_neighbors: Vec<(u32, CompactDirection)> = vec![
+            (2, CompactDirection::Incoming),
+            (3, CompactDirection::Outgoing),
+            (4, CompactDirection::Outgoing),
+        ];
+        let neighbors: Neighbors<u32, Directed> =
+            Neighbors::new(node_neighbors.iter(), PhantomData);
+
+        // Construct edges from `1`.
+        // The edge (2, 1) is being filtered out as the edges are directed.
+        let mut edges = Edges::new(from, &edges, neighbors);
+
+        // Test all existing edges from `1`.
+        assert_eq!(edges.next(), Some((1, 3, &3.0)));
+        assert_eq!(edges.next(), Some((1, 4, &4.0)));
+
+        // Test the end of iteration.
+        assert_eq!(edges.next(), None);
+    }
+
+    #[test]
+    #[should_panic]
+    fn edges_next_unreachable() {
+        // Prepare arguments.
+        let from: u32 = 1;
+        let edges: IndexMap<(u32, u32), f32> = IndexMap::new();
+        let node_neighbors: Vec<(u32, CompactDirection)> = vec![(2, CompactDirection::Incoming)];
+        let neighbors: Neighbors<u32, Directed> =
+            Neighbors::new(node_neighbors.iter(), PhantomData);
+
+        // Construct edges.
+        let mut edges = Edges::new(from, &edges, neighbors);
+
+        assert_eq!(edges.next(), Some((1, 2, &0.0)));
+    }
+
+    #[test]
+    fn all_edges_new() {
+        let _all_edges: AllEdges<u32, f32, Directed> =
+            AllEdges::new(IndexMap::new().iter(), PhantomData);
+    }
+
+    #[test]
+    fn all_edges_next() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+
+        let mut all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        assert_eq!(all_edges.next(), Some((2, 1, &2.0)));
+        assert_eq!(all_edges.next(), Some((1, 3, &3.0)));
+        assert_eq!(all_edges.next(), Some((1, 4, &4.0)));
+        assert_eq!(all_edges.next(), None);
+    }
+
+    #[test]
+    fn all_edges_size_hint() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+        let mut all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        assert_eq!(all_edges.size_hint(), (3, Some(3)));
+
+        // Lower the length of the iterator.
+        all_edges.next();
+
+        assert_eq!(all_edges.size_hint(), (2, Some(2)));
+
+        // Lower the length of the iterator.
+        all_edges.next();
+
+        assert_eq!(all_edges.size_hint(), (1, Some(1)));
+
+        // Lower the length of the iterator.
+        all_edges.next();
+
+        assert_eq!(all_edges.size_hint(), (0, Some(0)));
+    }
+
+    #[test]
+    fn all_edges_count() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+        let all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        assert_eq!(all_edges.count(), 3);
+    }
+
+    #[test]
+    fn all_edges_nth() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+        let mut all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        assert_eq!(all_edges.nth(2), Some((1, 4, &4.0)));
+        assert_eq!(all_edges.nth(0), None);
+    }
+
+    #[test]
+    fn all_edges_last() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+        let all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        assert_eq!(all_edges.last(), Some((1, 4, &4.0)));
+    }
+
+    #[test]
+    fn all_edges_next_back() {
+        let mut edges: IndexMap<(u32, u32), f32> = IndexMap::with_capacity(3);
+        edges.insert((2, 1), 2.0);
+        edges.insert((1, 3), 3.0);
+        edges.insert((1, 4), 4.0);
+
+        let mut all_edges: AllEdges<u32, f32, Directed> = AllEdges::new(edges.iter(), PhantomData);
+
+        // Iterate backwards.
+        assert_eq!(all_edges.next_back(), Some((1, 4, &4.0)));
+        assert_eq!(all_edges.next_back(), Some((1, 3, &3.0)));
+        assert_eq!(all_edges.next_back(), Some((2, 1, &2.0)));
+        assert_eq!(all_edges.next_back(), None);
+    }
+
+    #[test]
+    fn into_weighted_edge() {
+        // Test with tuple.
+        assert_eq!((1, 2).into_weighted_edge(), (1, 2, f32::default()));
+
+        // Test with triple.
+        assert_eq!((1, 2, 3).into_weighted_edge(), (1, 2, 3));
+
+        // Test with triple having edge weight as reference.
+        assert_eq!((1, 2, &3).into_weighted_edge(), (1, 2, 3));
+
+        // Test with tuple as reference.
+        assert_eq!((&(1, 2)).into_weighted_edge(), (1, 2, f32::default()));
+
+        // Test with triple as reference.
+        assert_eq!((&(1, 2, 3)).into_weighted_edge(), (1, 2, 3));
+    }
+
+    #[test]
+    fn direction_opposite() {
+        assert_eq!(Direction::Incoming.opposite(), Direction::Outgoing);
+        assert_eq!(Direction::Outgoing.opposite(), Direction::Incoming);
+    }
+
+    #[test]
+    fn direction_index() {
+        assert_eq!(Direction::Incoming.index(), Direction::Incoming as usize);
+        assert_eq!(Direction::Outgoing.index(), Direction::Outgoing as usize);
+    }
+
+    #[test]
+    fn direction_clone() {
+        assert_eq!(Direction::Incoming.clone(), Direction::Incoming);
+        assert_eq!(Direction::Outgoing.clone(), Direction::Outgoing);
+    }
+
+    #[test]
+    fn compact_direction_from() {
+        assert_eq!(
+            CompactDirection::from(Direction::Incoming),
+            CompactDirection::Incoming
+        );
+        assert_eq!(
+            CompactDirection::from(Direction::Outgoing),
+            CompactDirection::Outgoing
+        );
+    }
+
+    #[test]
+    fn compact_direction_partial_equal_with_direction() {
+        assert_eq!(CompactDirection::Incoming.eq(&Direction::Incoming), true);
+        assert_eq!(CompactDirection::Incoming.eq(&Direction::Outgoing), false);
+
+        assert_eq!(CompactDirection::Outgoing.eq(&Direction::Outgoing), true);
+        assert_eq!(CompactDirection::Outgoing.eq(&Direction::Incoming), false);
+    }
+}
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -2,3 +2,6 @@ pub mod graph;
 pub mod edge;
 pub mod node;
 pub mod traverse;
+
+pub use self::graph::{Directed, Graph, Undirected, UndirectedGraph};
+pub use self::node::NodeTrait;
