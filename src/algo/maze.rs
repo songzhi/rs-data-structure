db2fecf37129f29f4a03e72d274beeeebca1a78a
冬冬 李 <lsongzhi@163.com>
2019-03-30 09:04:33 +0800

    with_len layout generator

--- src/algo/maze.rs
+++ src/algo/maze.rs
@@ -1,15 +1,18 @@
 use random::Source;
 
 pub struct Maze {
-    layout: [[bool; 16]; 16],
+    layout: Vec<Vec<bool>>,
 }
 
 
 impl Maze {
     pub fn new() -> Self {
+        Self::with_len(16)
+    }
+    pub fn with_len(len: usize) -> Self {
         let mut source = random::default().seed([42, 69]);
         let mut sequence = source.iter::<i8>();
-        let mut layout = [[false; 16]; 16];
+        let mut layout = vec![vec![false; len]; len];
         for row in layout.iter_mut() {
             for elem in row.iter_mut() {
                 *elem = sequence.next().unwrap() >= 0;
