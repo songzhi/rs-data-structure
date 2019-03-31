126a65d5c7d51307c20fded3493a61be04a129fd
冬冬 李 <lsongzhi@163.com>
2019-03-31 17:02:56 +0800

    clean main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,5 @@
 #![allow(dead_code)]
 
 use rs_data_structure::*;
-use tree::bintree::BinTree;
-use tree::bin_search_tree::BinSearchTree;
 
-fn display_bin_tree() {
-    let seq = "ABC##DE#G##F###";
-    let tree = BinTree::from_seq_pre(seq.chars().into_iter(), &'#');
-    println!("{}", tree);
-}
-
-fn display_bin_search_tree() {
-    let mut tree = BinSearchTree::new();
-    tree.insert(5);
-    tree.insert(2);
-    tree.insert(3);
-    tree.insert(9);
-    tree.insert(9);
-    tree.insert(10);
-    println!("{}", tree);
-}
-
-fn main() {
-    display_bin_search_tree();
-    use expr::*;
-    use std::str::FromStr;
-    let infix_expr: Expr<Infix> = Expr::from_str("1+2*(5-3)").unwrap();
-    let pre :Expr<Prefix> = infix_expr.into();
-    println!("{}", format!("{}", pre));
-}
+fn main() {}
