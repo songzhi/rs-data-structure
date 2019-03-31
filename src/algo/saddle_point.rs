2730fe67d2673c210194345a0abb381b1fd6dadc
冬冬 李 <lsongzhi@163.com>
2019-03-31 22:23:37 +0800

    refactor saddle_point

--- src/algo/saddle_point.rs
+++ src/algo/saddle_point.rs
@@ -1,21 +1,5 @@
-use std::collections::HashSet;
-use std::hash::{Hash, Hasher};
-
-struct Pos(usize, usize);
-
-impl Hash for Pos {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        Hash::hash_slice(&[self.0, self.1], state);
-    }
-}
-
-impl PartialEq for Pos {
-    fn eq(&self, other: &Self) -> bool {
-        self.0 == other.0 && self.1 == other.1
-    }
-}
-
-impl Eq for Pos {}
+//! 鞍点问题: 若矩阵 A 中的某一元素 A[i,j]是第 i 行中的最小值，而又是第 j 列中的最 大值，则称 A[i,j]是矩阵 A 中的一个鞍点。
+//!
 
 pub fn saddle_points<T: Ord>(matrix: &Vec<Vec<T>>) -> Vec<(usize, usize)> {
     let row_mins: Vec<(usize, usize)> = matrix.iter().enumerate().map(|(i, row)| {
