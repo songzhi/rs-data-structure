3ab511601216809db686055ee5654fd7de8b224a
lsongzhi <lsongzhi@163.com>
2019-09-24 21:03:19 +0800

    refactor

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -2,7 +2,6 @@ pub mod binary_tree;
 pub mod binary_search_tree;
 pub mod huffman_tree;
 pub mod avl_tree;
-pub mod btree;
 pub use self::binary_tree::BinaryTree;
 pub use self::binary_search_tree::BinarySearchTree;
 pub use self::huffman_tree::HuffmanTree;
