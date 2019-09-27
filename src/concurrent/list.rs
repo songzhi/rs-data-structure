6fc6ec0266ecec6d4ade8e59f9099def38c65ed8
lsongzhi <lsongzhi@163.com>
2019-09-27 14:27:23 +0800

    fix

--- src/concurrent/list.rs
+++ src/concurrent/list.rs
@@ -1,14 +1,17 @@
-use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicPtr, Ordering};
+use std::mem::ManuallyDrop;
+use std::ptr::NonNull;
+use owned_alloc::OwnedAlloc;
 
 pub struct Node<T> {
-    elem: T,
+    elem: ManuallyDrop<T>,
     next: Link<T>,
 }
 
 impl<T> Node<T> {
     pub fn new(elem: T) -> Self {
         Node {
-            elem,
+            elem: ManuallyDrop::new(elem),
             next: AtomicPtr::default(),
         }
     }
@@ -19,42 +22,83 @@ type Link<T> = AtomicPtr<Node<T>>;
 #[derive(Default)]
 pub struct List<T> {
     head: Link<T>,
-    count: AtomicUsize,
 }
 
-unsafe impl<T> Send for List<T> {}
+unsafe impl<T> Send for List<T> where T: Send {}
+
+unsafe impl<T> Sync for List<T> where T: Send {}
 
 impl<T> List<T> {
-    #[inline]
-    pub fn len(&self) -> usize {
-        self.count.load(Ordering::Acquire)
-    }
-    #[inline]
-    pub fn is_empty(&self) -> bool {
-        self.len() == 0
-    }
-    pub fn push(&mut self, elem: T) {
-        let head = self.head.load(Ordering::Relaxed);
-        let mut node = Node::new(elem);
+    pub fn push(&self, elem: T) {
+        let node = OwnedAlloc::new(Node::new(elem)).into_raw();
+        let mut head = self.head.load(Ordering::Relaxed);
         loop {
-            node.next.store(head, Ordering::Relaxed);
-            if self.head.compare_exchange(head, &mut node, Ordering::Release, Ordering::Relaxed).is_ok() {
-                self.count.fetch_add(1, Ordering::AcqRel);
-                break;
+            unsafe { node.as_ref().next.store(head, Ordering::Relaxed); }
+            match self.head.compare_exchange(head, node.as_ptr(), Ordering::Release, Ordering::Relaxed) {
+                Ok(_) => {
+                    break;
+                }
+                Err(ptr) => head = ptr
             }
         }
     }
-    pub fn pop(&mut self) -> Option<T> {
+    pub fn pop(&self) -> Option<T> {
         unsafe {
-            let head = self.head.load(Ordering::Relaxed);
-            while !head.is_null() && self.head.compare_exchange(head, (*head).next.load(Ordering::Relaxed), Ordering::Release, Ordering::Relaxed).is_err() {
-                continue;
-            }
-            if head.is_null() {
-                None
-            } else {
-                Some(head.read().elem)
+            let mut head = NonNull::new(self.head.load(Ordering::Relaxed))?;
+            loop {
+                match self.head.compare_exchange(head.as_ptr(), head.as_ref().next.load(Ordering::SeqCst), Ordering::Release, Ordering::Relaxed) {
+                    Ok(_) => {
+                        head.as_ptr().drop_in_place();
+                        break Some((&mut *head.as_mut().elem as *mut T).read());
+                    }
+                    Err(ptr) => head = NonNull::new(ptr)?
+                }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::{sync::Arc, thread};
+
+
+    #[test]
+    fn no_data_corruption() {
+        const NTHREAD: usize = 20;
+        const NITER: usize = 500;
+        const NMOD: usize = 55;
+
+        let stack = Arc::new(List::default());
+        let mut handles = Vec::with_capacity(NTHREAD);
+
+        for i in 0..NTHREAD {
+            let stack = stack.clone();
+            handles.push(thread::spawn(move || {
+                for j in 0..NITER {
+                    let val = (i * NITER) + j;
+                    stack.push(val);
+                    if (val + 1) % NMOD == 0 {
+                        if let Some(val) = stack.pop() {
+                            assert!(val < NITER * NTHREAD);
+                        }
+                    }
+                }
+            }));
+        }
+
+        for handle in handles {
+            handle.join().expect("thread failed");
+        }
+
+        let expected = NITER * NTHREAD - NITER * NTHREAD / NMOD;
+        let mut res = 0;
+        while let Some(val) = stack.pop() {
+            assert!(val < NITER * NTHREAD);
+            res += 1;
+        }
+
+        assert_eq!(res, expected);
     }
 }
