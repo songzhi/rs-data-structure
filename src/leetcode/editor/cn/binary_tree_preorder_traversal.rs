2cc59b5dc368463aafdddd546226752eb7e59fb2
lsongzhi <lsongzhi@163.com>
2020-06-01 16:13:36 +0800

    feat(leetcode)

---
+++ src/leetcode/editor/cn/binary_tree_preorder_traversal.rs
@@ -0,0 +1,64 @@
+//! [144]二叉树的前序遍历
+use std::cell::RefCell;
+use std::rc::Rc;
+
+//给定一个二叉树，返回它的 前序 遍历。
+//
+// 示例:
+//
+// 输入: [1,null,2,3]
+//   1
+//    \
+//     2
+//    /
+//   3
+//
+//输出: [1,2,3]
+//
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+// Related Topics 栈 树
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub struct Solution;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+// Definition for a binary tree node.
+
+impl Solution {
+    pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        if let Some(node) = root {
+            let mut stack = vec![node];
+            let mut output = vec![];
+            while let Some(node) = stack.pop() {
+                output.push(node.borrow().val);
+                if let Some(right) = node.borrow().right.clone() {
+                    stack.push(right);
+                }
+                if let Some(left) = node.borrow().left.clone() {
+                    stack.push(left);
+                }
+            }
+            output
+        } else {
+            vec![]
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
