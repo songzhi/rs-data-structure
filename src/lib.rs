93af4b012ee10f638555909cadee0531f4537637
lsongzhi <lsongzhi@163.com>
2020-08-14 19:41:13 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -8,25 +8,18 @@
 
 use std::io::{BufRead, Read, Write};
 
-pub mod algo;
-// pub mod deque;
-// pub mod list;
-// pub mod utils;
-// pub mod vec;
-// //pub mod hash;
-// pub mod expr;
-// pub mod graph;
-// pub mod heap;
-// pub mod tree;
+pub mod deque;
+pub mod list;
+pub mod utils;
+pub mod vec;
+//pub mod hash;
+pub mod expr;
+pub mod graph;
+pub mod heap;
+pub mod tree;
 // generalized list
-// pub mod concurrent;
-// pub mod glist;
-pub mod leetcode;
-// pub mod skiplist;
-// pub mod sync;
+pub mod concurrent;
+pub mod glist;
+pub mod skiplist;
+pub mod sync;
 
-fn test_it(f: impl Fn(&[u8], &mut Vec<u8>), input: &str, output: &str) {
-    let mut buf: Vec<u8> = vec![];
-    f(input.as_bytes(), &mut buf);
-    assert_eq!(output, unsafe { String::from_utf8_unchecked(buf) });
-}
